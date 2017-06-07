a48ff4122442230252933662c9eea5d45d6c5c25
Stephen Bussey <sb8244@cs.ship.edu>
2017-06-06 23:56:35 -0400

    More validity checks for moves

--- lib/generals/board/command.ex
+++ lib/generals/board/command.ex
@@ -1,18 +1,24 @@
 defmodule Generals.Board.Command do
-  defstruct coords: {nil,nil}, type: nil, player: nil
+  defstruct from: {nil, nil}, to: {nil,nil}, type: nil, player: nil
 
   alias Generals.Board
 
-  def get_move_command(player: player, coords: coords = {_,_}, board: board = %Board{}) do
-    case valid_move?(board, coords) do
-      true -> %__MODULE__{coords: coords, type: :move, player: player}
-      false -> get_move_command(player: nil, coords: nil, board: nil)
+  def get_move_command(player: player, from: from = {_,_}, to: coords = {_,_}, board: board = %Board{}) do
+    case valid_move?(board, from, coords) do
+      true -> %__MODULE__{from: from, to: coords, type: :move, player: player}
+      false -> get_move_command(player: nil, from: nil, to: nil, board: nil)
     end
   end
-  def get_move_command(player: _, coords: _, board: _), do: {:error, "Invalid move coordinates"}
+  def get_move_command(player: _, from: _, to: _, board: _), do: {:error, "Invalid move coordinates"}
 
-  defp valid_move?(board = %{dimensions: dimensions}, coords) do
-    Board.Dimensions.valid_coords?(dimensions, coords) &&
-    Board.Cell.moveable?(Board.at(board, coords))
+  defp valid_move?(board = %{dimensions: dimensions}, from, to) do
+    Board.Dimensions.valid_coords?(dimensions, to) &&
+    Board.Dimensions.valid_coords?(dimensions, from) &&
+    Board.Cell.moveable?(Board.at(board, to)) &&
+    manhatten_distance(from, to) == 1
+  end
+
+  defp manhatten_distance({r,c}, {r2,c2}) do
+    abs(r - r2) + abs(c - c2)
   end
 end
--- test/generals/board/command_test.exs
+++ test/generals/board/command_test.exs
@@ -9,35 +9,48 @@ defmodule Generals.Board.CommandTest do
 
     setup do
       board = Generals.Board.get_new(rows: 3, columns: 3)
-        |> Board.replace_cell({1,1}, %Board.Cell{ row: 1, column: 1, type: :mountain })
+      mountain_board = Board.replace_cell(board, {1,1}, %Board.Cell{ row: 1, column: 1, type: :mountain })
 
-      {:ok, board: board}
+      {:ok, board: board, mountain_board: mountain_board}
     end
 
     test "a valid command is returned", %{ board: board } do
-      assert Command.get_move_command(player: 1, coords: {0, 1}, board: board) == %Command{
+      assert Command.get_move_command(player: 1, from: {0,0}, to: {0, 1}, board: board) == %Command{
         player: 1,
-        coords: {0,1},
+        from: {0,0},
+        to: {0,1},
         type: :move
       }
     end
 
     test "an invalid coord will be an error", %{ board: board } do
-      assert Command.get_move_command(player: 1, coords: {1}, board: board) == @invalid
+      assert Command.get_move_command(player: 1, from: {0,0}, to: {1}, board: board) == @invalid
     end
 
     test "the r,c must be inside of the dimensions", %{ board: board } do
-      assert Command.get_move_command(player: 1, coords: {-1,0}, board: board) == @invalid
-      assert Command.get_move_command(player: 1, coords: {3,1}, board: board) == @invalid
-      assert Command.get_move_command(player: 1, coords: {1,3}, board: board) == @invalid
-      assert Command.get_move_command(player: 1, coords: {0,-1}, board: board) == @invalid
+      assert Command.get_move_command(player: 1, from: {0,0}, to: {-1,0}, board: board) == @invalid
+      assert Command.get_move_command(player: 1, from: {2,1}, to: {3,1}, board: board) == @invalid
+      assert Command.get_move_command(player: 1, from: {1,2}, to: {1,3}, board: board) == @invalid
+      assert Command.get_move_command(player: 1, from: {0,0}, to: {0,-1}, board: board) == @invalid
 
-      assert Command.get_move_command(player: 1, coords: {0,0}, board: board) != @invalid
-      assert Command.get_move_command(player: 1, coords: {2,2}, board: board) != @invalid
+      assert Command.get_move_command(player: 1, from: {0,1}, to: {0,0}, board: board) != @invalid
+      assert Command.get_move_command(player: 1, from: {2,1}, to: {2,2}, board: board) != @invalid
     end
 
-    test "the move cannot be made onto a mountain", %{board: board} do
-      assert Command.get_move_command(player: 1, coords: {1,1}, board: board) == @invalid
+    test "the from and to must be manhattan adjacent", %{board: board} do
+      assert Command.get_move_command(player: 1, from: {0,0}, to: {1,1}, board: board) == @invalid
+      assert Command.get_move_command(player: 1, from: {2,2}, to: {1,1}, board: board) == @invalid
+      assert Command.get_move_command(player: 1, from: {0,2}, to: {1,1}, board: board) == @invalid
+      assert Command.get_move_command(player: 1, from: {2,0}, to: {1,1}, board: board) == @invalid
+
+      assert Command.get_move_command(player: 1, from: {0,1}, to: {1,1}, board: board) != @invalid
+      assert Command.get_move_command(player: 1, from: {1,0}, to: {1,1}, board: board) != @invalid
+      assert Command.get_move_command(player: 1, from: {2,1}, to: {1,1}, board: board) != @invalid
+      assert Command.get_move_command(player: 1, from: {1,2}, to: {1,1}, board: board) != @invalid
+    end
+
+    test "the move cannot be made onto a mountain", %{mountain_board: board} do
+      assert Command.get_move_command(player: 1, from: {0,1}, to: {1,1}, board: board) == @invalid
     end
   end
 end
