a8010ab11c9dfbe81b0bbb479a86f54f24045d19
Stephen Bussey <sb8244@cs.ship.edu>
2017-06-09 01:50:50 -0400

    Move from one space to another

--- lib/generals/board/command.ex
+++ lib/generals/board/command.ex
@@ -3,6 +3,9 @@ defmodule Generals.Board.Command do
 
   alias Generals.Board
 
+  @invalid_ownership_error "Cannot move from a space you don't hold"
+  @invalid_move_error      "Cannot move to this space"
+
   def get_move_command(player: player, from: from = {_,_}, to: coords = {_,_}, board: board = %Board{}) do
     case validate_move(board, from, coords, player) do
       true -> %__MODULE__{from: from, to: coords, type: :move, player: player}
@@ -11,6 +14,13 @@ defmodule Generals.Board.Command do
   end
   def get_move_command(player: _, from: _, to: _, board: _), do: {:error, "Cannot move to this space"}
 
+  def execute(command = %__MODULE__{player: player, from: from}, board = %Board{}) do
+    case Board.Cell.owned_by?(Board.at(board, from), player) do
+      false -> {:error, @invalid_ownership_error}
+      true -> {:ok, perform_move(command, board)}
+    end
+  end
+
   defp validate_move(board = %{dimensions: dimensions}, from, to, player) do
     valid_movement = Board.Dimensions.valid_coords?(dimensions, to) &&
                      Board.Dimensions.valid_coords?(dimensions, from) &&
@@ -18,15 +28,53 @@ defmodule Generals.Board.Command do
                      manhatten_distance(from, to) == 1
 
     case valid_movement do
-      false -> {:error, "Cannot move to this space"}
+      false -> {:error, @invalid_move_error}
       true ->
         valid_ownership = Board.Cell.owned_by?(Board.at(board, from), player)
         case valid_ownership do
-          false -> {:error, "Cannot move from a space you don't hold"}
+          false -> {:error, @invalid_ownership_error}
           true -> true
         end
     end
   end
+
+  defp perform_move(%__MODULE__{ from: from, to: to }, board) do
+    from_cell = Board.at(board, from)
+    to_cell = Board.at(board, to)
+
+    case compute_move(from_cell.owner == to_cell.owner, from_armies: from_cell.population_count - 1, to_armies: to_cell.population_count) do
+      {:from, new_to_population} ->
+        board
+          |> Board.replace_cell(from, Map.merge(from_cell, %{ population_count: 1 }))
+          |> Board.replace_cell(to, Map.merge(to_cell, %{ population_count: new_to_population, owner: from_cell.owner }))
+          |> convert_general(to_cell.type, to_coords: to, from_owner: to_cell.owner, to_owner: from_cell.owner)
+      {:to, new_to_population} ->
+        board
+          |> Board.replace_cell(from, Map.merge(from_cell, %{ population_count: 1 }))
+          |> Board.replace_cell(to, Map.merge(to_cell, %{ population_count: new_to_population }))
+    end
+  end
+
+  defp compute_move(true, from_armies: from_armies, to_armies: to_armies)
+    when from_armies > 0,
+    do: {:from, from_armies + to_armies}
+  defp compute_move(false, from_armies: from_armies, to_armies: to_armies)
+    when from_armies > 0 and from_armies > to_armies,
+    do: {:from, from_armies - to_armies}
+  defp compute_move(false, from_armies: from_armies, to_armies: to_armies)
+    when from_armies > 0 and to_armies > from_armies,
+    do: {:to, to_armies - from_armies}
+  defp compute_move(false, from_armies: from_armies, to_armies: to_armies)
+    when from_armies > 0 and to_armies == from_armies,
+    do: {:to, 0}
+
+  defp convert_general(board, :general, to_coords: to_coords, from_owner: from, to_owner: to) do
+    to_cell = Board.at(board, to_coords)
+    board
+      |> Board.replace_cell(to_coords, Map.merge(to_cell, %{ type: :town }))
+      |> Board.convert_owner_on_all_owned_cells(from: from, to: to)
+  end
+  defp convert_general(board, _, _), do: board
 
   defp manhatten_distance({r,c}, {r2,c2}) do
     abs(r - r2) + abs(c - c2)
--- test/generals/board/command_test.exs
+++ test/generals/board/command_test.exs
@@ -64,4 +64,81 @@ defmodule Generals.Board.CommandTest do
       assert Command.get_move_command(player: 1, from: {0,1}, to: {1,1}, board: board) == @invalid
     end
   end
+
+  describe "execute_command/2" do
+    setup do
+      board = Generals.Board.get_new(rows: 3, columns: 3)
+        |> Board.replace_cell({1,1}, %Board.Cell{ row: 1, column: 1, type: :plains, owner: 1, population_count: 10 })
+      {:ok, board: board}
+    end
+
+    test "moving from a location which you don't own is an error", %{board: board} do
+      command = Command.get_move_command(player: 1, from: {1,1}, to: {1,2}, board: board)
+      unowned_space_board = Board.replace_cell(board, {1,1}, %Board.Cell{ row: 1, column: 1, type: :plains, owner: 2 })
+      assert Command.execute(command, unowned_space_board) == {:error, "Cannot move from a space you don't hold"}
+    end
+
+    test "moving onto a plains -> gains n-1 army on plains, leaves 1 army behind", %{board: board} do
+      command = Command.get_move_command(player: 1, from: {1,1}, to: {1,2}, board: board)
+      {:ok, new_board} = Command.execute(command, board)
+      assert Board.at(new_board, {1,1}) == %Board.Cell{row: 1, column: 1, type: :plains, owner: 1, population_count: 1}
+      assert Board.at(new_board, {1,2}) == %Board.Cell{row: 1, column: 2, type: :plains, owner: 1, population_count: 9}
+    end
+
+    # test "moving with a percentage will split the army up, moving only the percentage to the plain"
+
+    test "moving onto a town which is less health will overtake the town for the player", %{board: board} do
+      command = Command.get_move_command(player: 1, from: {1,1}, to: {1,2}, board: board)
+      check_board = board
+        |> Board.replace_cell({1,1}, %Board.Cell{ row: 1, column: 1, type: :plains, owner: 1, population_count: 10 })
+        |> Board.replace_cell({1,2}, %Board.Cell{ row: 1, column: 2, type: :town, owner: 2, population_count: 5 })
+      {:ok, new_board} = Command.execute(command, check_board)
+      assert Board.at(new_board, {1,1}) == %Board.Cell{row: 1, column: 1, type: :plains, owner: 1, population_count: 1}
+      assert Board.at(new_board, {1,2}) == %Board.Cell{row: 1, column: 2, type: :town, owner: 1, population_count: 4}
+    end
+
+    test "moving onto a town which is more health will leave the town with reduced population", %{board: board} do
+      command = Command.get_move_command(player: 1, from: {1,1}, to: {1,2}, board: board)
+      check_board = board
+        |> Board.replace_cell({1,1}, %Board.Cell{ row: 1, column: 1, type: :plains, owner: 1, population_count: 5 })
+        |> Board.replace_cell({1,2}, %Board.Cell{ row: 1, column: 2, type: :town, owner: 2, population_count: 10 })
+      {:ok, new_board} = Command.execute(command, check_board)
+      assert Board.at(new_board, {1,1}) == %Board.Cell{row: 1, column: 1, type: :plains, owner: 1, population_count: 1}
+      assert Board.at(new_board, {1,2}) == %Board.Cell{row: 1, column: 2, type: :town, owner: 2, population_count: 6}
+    end
+
+    test "moving onto a town which is equal health will leave the town with 0 population", %{board: board} do
+      command = Command.get_move_command(player: 1, from: {1,1}, to: {1,2}, board: board)
+      check_board = board
+        |> Board.replace_cell({1,1}, %Board.Cell{ row: 1, column: 1, type: :plains, owner: 1, population_count: 6 })
+        |> Board.replace_cell({1,2}, %Board.Cell{ row: 1, column: 2, type: :town, owner: 2, population_count: 5 })
+      {:ok, new_board} = Command.execute(command, check_board)
+      assert Board.at(new_board, {1,1}) == %Board.Cell{row: 1, column: 1, type: :plains, owner: 1, population_count: 1}
+      assert Board.at(new_board, {1,2}) == %Board.Cell{row: 1, column: 2, type: :town, owner: 2, population_count: 0}
+    end
+
+    test "moving onto a town owned by the same player will combine the forces", %{board: board} do
+      command = Command.get_move_command(player: 1, from: {1,1}, to: {1,2}, board: board)
+      check_board = board
+        |> Board.replace_cell({1,1}, %Board.Cell{ row: 1, column: 1, type: :plains, owner: 1, population_count: 6 })
+        |> Board.replace_cell({1,2}, %Board.Cell{ row: 1, column: 2, type: :town, owner: 1, population_count: 5 })
+      {:ok, new_board} = Command.execute(command, check_board)
+      assert Board.at(new_board, {1,1}) == %Board.Cell{row: 1, column: 1, type: :plains, owner: 1, population_count: 1}
+      assert Board.at(new_board, {1,2}) == %Board.Cell{row: 1, column: 2, type: :town, owner: 1, population_count: 10}
+    end
+
+    test "moving onto an enemy general which is less health will take over the general, turning it into a town and converting all enemy pieces to the current player", %{board: board} do
+      command = Command.get_move_command(player: 1, from: {1,1}, to: {1,2}, board: board)
+      check_board = board
+        |> Board.replace_cell({1,1}, %Board.Cell{ row: 1, column: 1, type: :plains, owner: 1, population_count: 10 })
+        |> Board.replace_cell({1,2}, %Board.Cell{ row: 1, column: 2, type: :general, owner: 2, population_count: 5 })
+        |> Board.replace_cell({0,0}, %Board.Cell{ row: 0, column: 0, type: :town, owner: 2, population_count: 5 })
+        |> Board.replace_cell({1,0}, %Board.Cell{ row: 1, column: 0, type: :plains, owner: 2, population_count: 6 })
+      {:ok, new_board} = Command.execute(command, check_board)
+      assert Board.at(new_board, {1,1}) == %Board.Cell{row: 1, column: 1, type: :plains, owner: 1, population_count: 1}
+      assert Board.at(new_board, {1,2}) == %Board.Cell{row: 1, column: 2, type: :town, owner: 1, population_count: 4}
+      assert Board.at(new_board, {0,0}) == %Board.Cell{row: 0, column: 0, type: :town, owner: 1, population_count: 5}
+      assert Board.at(new_board, {1,0}) == %Board.Cell{row: 1, column: 0, type: :plains, owner: 1, population_count: 6}
+    end
+  end
 end
