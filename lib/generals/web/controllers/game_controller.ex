2af246b038a72d89ebd98b093510bc422d06d630
Stephen Bussey <sb8244@cs.ship.edu>
2017-06-11 23:02:27 -0400

    Refactor controller work so far
    Plug interface is really cool

--- lib/generals/web/controllers/game_controller.ex
+++ lib/generals/web/controllers/game_controller.ex
@@ -1,27 +1,26 @@
 defmodule Generals.Web.GameController do
   use Generals.Web, :controller
 
+  plug :ensure_user_id
+
   def index(conn, _params) do
-    ensure_user_id(conn)
-      |> render("index.html")
+    render(conn, "index.html")
   end
 
   def create(conn, _params) do
-    conn = ensure_user_id(conn)
-    case Generals.GamesSupervisor.create_game(user_ids: [get_user_id(conn)]) do
+    case Generals.Game.create_game(user_ids: [get_user_id(conn)]) do
       {:ok, id, _pid} -> redirect(conn, to: game_path(conn, :show, id))
     end
   end
 
   def show(conn, %{"id" => id}) do
-    conn = ensure_user_id(conn)
-    case Generals.GamesSupervisor.get_game(id) do
-      pid when is_pid(pid) -> text(conn, inspect({id, get_session(conn, :user_id)}))
-      nil -> text(conn, "This is not a game")
+    case Generals.Game.find_user_game(game_id: id, user_id: get_user_id(conn)) do
+      {:ok, _} -> text(conn, inspect({id, get_session(conn, :user_id)}))
+      {:error, why} -> text(conn, why)
     end
   end
 
-  defp ensure_user_id(conn) do
+  defp ensure_user_id(conn, _p) do
     case get_user_id(conn) do
       nil -> put_session(conn, :user_id, UUID.uuid4())
       _ -> conn
